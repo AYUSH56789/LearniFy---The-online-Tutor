e5cf085817f48a5928fca231464c8dec217d777f
dhruvgirdhar07 <girdhardhruv07@gmail.com>
2024-08-15 19:48:11 +0530

    footer commit 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar';
 import './App.css' ;
-import Home from './components/home/Home';
 import { Outlet } from 'react-router-dom';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <>
     <Navbar/>
     <Outlet/>
+    <Footer/>
     </>
     
   )
---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import './Footer.css';
+import flamingoLogo from "./flamingo_logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-section address">
+          <h2>Address</h2>
+          <p>1st Floor, Opp GNE COLLEGE,</p>
+          <p>Gill Road, Ludhiana - 141003 (Main Gate)</p>
+          <p>Phone: 9779945798</p>
+          <p>Mail: flamingo121@gmail.com</p>
+        </div>
+        <div className="footer-section logo">
+          <img className="foot-logo" src={flamingoLogo} alt="Flamingo" />
+        </div>
+        <div className="footer-section quick-links">
+          <h2>Quick Link</h2>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/courses">Courses</Link></li>
+            <li><Link to="/mocktest">Mock-Test</Link></li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>COPYRIGHT © 2023 - Flamingo | ALL RIGHTS RESERVED</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
