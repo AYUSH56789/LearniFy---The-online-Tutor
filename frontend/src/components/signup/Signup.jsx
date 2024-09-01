1c343a1adf76e79d9baca10ec69852e0c1bb52ff
dhruvgirdhar07 <girdhardhruv07@gmail.com>
2024-09-01 12:49:32 +0530

    signup 1st commit

--- frontend/src/components/signup/Signup.jsx
+++ frontend/src/components/signup/Signup.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import "./Signup.css" 
 function Signup() {
   return (
-    <div>
-      <h2>this is signup page</h2>
+    <div className="signup-container">
+    <div className="signup-form">
+      <h2>Sign-Up</h2>
+      <form>
+        <input type="text" placeholder="Enter Name" required />
+        <input type="email" placeholder="Enter Email" required />
+        <input type="password" placeholder="Enter Password" required />
+        <button type="submit">Log in</button>
+      </form>
     </div>
+    <div className="signup-image">
+      <img src="signupimg.jpg" alt="Signup Illustration" />
+    </div>
+  </div>
   )
 }
 
